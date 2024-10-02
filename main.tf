4597ce3e2cdfd91766c2865b101ca7d8f78f71e4
nagswe <126598198+nagswe@users.noreply.github.com>
2024-10-02 21:55:36 +0530

    yes

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-region = ""
-access_key = ""
-secret_key = ""
+region = "eu-west-2"
+access_key = "AKIAZJKDOM7KVWLN3BG7"
+secret_key = "xGmDW13VZxQqaxqyw9ZcHvPaS2zK7d9jQbyEHgiD"
 }
 
 resource "aws_instance" "ec2_example" {
-  ami                    = ""
+  ami                    = "ami-03ceeb33c1e4abcd1"
   instance_type          = var.instance_type
   key_name = "dockerswarm"
   tags = {
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2_example" {
 }
 
 resource "aws_ebs_volume" "example" {
-  availability_zone = ""
+  availability_zone = "eu-west-2a"
   size              = 40
 
   tags = {
